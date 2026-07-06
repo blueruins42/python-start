2834ec38ed5043ca69760708ed2c4574898b3092
blueruins42 <blueruins42@gmail.com>
2026-07-06 22:30:22 +0800

    Add variable creation and usage examples in Python

---
+++ variables-create-use-name.py
@@ -0,0 +1,14 @@
+# create a variable
+var= 1
+print(var)
+
+# use a variable
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+
+# combine text and variable using + operator
+var = "3.8.5"
+print("Python version: " + var)
+
