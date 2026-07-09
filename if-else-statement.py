55d8c3c27108102db923f5d62d2b2862233178f4
blueruins42 <blueruins42@gmail.com>
2026-07-09 22:33:25 +0800

    Implement if-else statements for weather-based activities

---
+++ if-else-statement.py
@@ -0,0 +1,30 @@
+# questions and answers
+n = int(input("Enter a number:"))
+print(n >= 100)
+
+# if statement
+if the_weather_is_good:
+    go_for_a_walk()
+have_lunch()
+
+# if/else statement
+if the_weather_is_good:
+    go_for_a_walk()
+    have_fun()
+else: 
+    go_to_a_theatre()
+    watch_movies()
+have_lunch()
+
+# nested if/else statement
+if the_weather_is_good:
+    if nice_restaurant_is_found:
+        have_lunch()
+    else:
+        eat_a_sandwich()
+else:
+    if tickets_are_available:
+        go_to_a_theatre()
+    else:
+        go_shopping()
+      
